2bb39ff0b3cab4025a524bcb55ebc46b1449732f
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-02-13 01:08:07 +0600

    windows intial

---
+++ AIUB/semester 1/LAB 3/TASK1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x;
+    cout << "Even/Odd checker : " ;
+    cin >> x;
+    if (x%2 == 0){
+        cout << x << " is an even number." << endl;
+    }else
+        cout << x << " is an odd number." << endl;
+}
+
+
+
---
+++ AIUB/semester 2/JAVA/LAB 1/Homework.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Homework
+{
+	public static void main(String args[]){
+		System.out.println("My name is : Ahmed, Md Shamim");
+		System.out.println("My ID is : 20-44242-3");
+		System.out.println("My current semester is : Second");
+	}
+}
