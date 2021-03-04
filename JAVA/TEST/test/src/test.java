31f8a39ba13f645ef519bb626b5cc3d5a75975c6
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-03-04 16:51:46 +0600

    04 mar

---
+++ JAVA/TEST/test/src/test.java
@@ -0,0 +1,13 @@
+public class test {
+    public int x;
+    public static void main(String[] args) {
+        String s1 = "Shamim";
+        String s2 = "Ahmed";
+        String s3 = s1.substring(2,4);
+        System.out.println(s3);
+        test obj = new test();
+        System.out.println(obj.x);
+
+            
+    }
+}
