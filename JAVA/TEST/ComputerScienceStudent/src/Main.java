41bb1b52cf99242125b402fa1b69d3006579b506
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-03-21 03:20:26 +0600

    removing all files

--- JAVA/TEST/ComputerScienceStudent/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ComputerScienceStudent csStudentObj = new ComputerScienceStudent("Computer Science");
-        csStudentObj.describeStudent("Tonmoy",20,"112",4.00);
-        csStudentObj.displayInfo();
-    }
-
-    // alt + 1  or shift + esc
-}
