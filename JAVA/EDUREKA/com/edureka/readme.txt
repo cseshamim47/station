7de8c7fb6feb8d34c9fd1ac6ce59c05b44800d82
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-03-21 02:47:11 +0600

    test L

--- JAVA/EDUREKA/com/edureka/readme.txt
+++
@@ -1,3 +0,0 @@
-javac -cp . com/package_name/*.java // -cp : class path
-javac -d output_directory -cp . com/package_name/*.java
-java -cp . com.package_name.main_class_name
--- JAVA/EDUREKA/com/edureka/test.java
+++
@@ -1,8 +0,0 @@
-package com.edureka;
-
-public class test{
-    public static void main(String[] args){
-        helloWorld obj = new helloWorld();
-        obj.test();
-    }
-}
