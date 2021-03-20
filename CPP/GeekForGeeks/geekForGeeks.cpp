25b72320ddd1853e8138da32330a3461a3af6a88
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-03-21 05:17:02 +0600

    GFG cpp 5:16am

--- CPP/GeekForGeeks/geekForGeeks.cpp
+++ CPP/GeekForGeeks/geekForGeeks.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
-// #include <algorithm>
 using namespace std;
 
 int main()
 {
-    int n,k;
-    cin >> n >> k;
-    int answer;
-    int x = min(n,k);
-    for(int i = 1; i <= x; i++ ){
-        if(n%i==0 and k%i==0){
-            answer = i;
-        }
-    }    
-    cout << answer;
+    int x;
+    cin >> x;
+    cout << "1 1 ";
+    int a = 1, b = 1, c = 0;
+    for(int i = 2; i<=x; i++){
+        c = a+b;
+        cout << c << " ";
+        a = b;
+        b = c;
+        
+    }
+    // cout << c << endl;
+    
 }
