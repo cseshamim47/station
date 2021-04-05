22d84632f7f3a71d337a44a4c6a24731f7ebffc9
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-04-05 22:54:02 +0600

    test

--- CPP/GeekForGeeks/geekForGeeks.cpp
+++ CPP/GeekForGeeks/geekForGeeks.cpp
@@ -1,76 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-void method(vector<vector<int>> M, int N){
-    int count = 0;
-        int ans = 0;
-    
-        for(int i = 1; i < N; i++){
-            for(int j = i-1; j >= 0; j--){
-                 if(M[0][i] == M[0][j]) M[0][i] = -1;
+
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+class Solution
+{   
+public:     
+    void transpose(vector<vector<int> >& matrix, int n)
+    { 
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                int swap = matrix[i][j];
+                matrix[j][i] = matrix[]
+            }
+        } 
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+        int n;
+        cin>>n;
+        vector<vector<int> > matrix(n); 
+
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].assign(n, 0);
+            for( int j=0; j<n; j++)
+            {
+                cin>>matrix[i][j];
             }
         }
-        
-        // for(int i = 0; i < N; i++){
-        // for(int j = 0; j < N; j++){
-        //     cout << M[i][j] << " ";
-        // }
-        // cout << endl;
-        // }
+
+        Solution ob;
+        ob.transpose(matrix,n);
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < n; ++j){
+                cout<<matrix[i][j]<<" ";
+            }
+            cout << endl;
+        }    
+
+        cout<<endl;
     
-    
-       for(int i = 0; i < N; i++){
-           count = 1;
-           for(int j = 1; j < N; j++){
-                for(int k = 0; k < N; k++){
-                       if(M[0][i] == M[j][k]){
-                           count++;
-                           cout << count << endl;
-                           break;
-                       }
-                }
-           }
-          if(count == N) ans++; 
-          
-       }
-       cout << ans;
-}
-
-int main()
-{
-    int N;
-    cin >> N;
-    vector <vector <int> > M(N, vector<int>(N, 0));
-
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
-            cin >> M[i][j];
-        }
-    }
-
-
-
-    method(M,N);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // for(int i = 0; i < N; i++){
-    //     for(int j = 0; j < N; j++){
-    //         cout << M[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    
-    
-    cout << endl;
-}
+    return 0;
+}  // } Driver Code Ends
