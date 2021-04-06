de150fb9693a647b7f9124cb1d6d5e1933da2ae7
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-04-06 16:38:54 +0600

    Tonmoy lab done

--- AIUB/semester 2/JAVA/Tonmoy/LAB_10/Task_5/src/Start.java
+++ AIUB/semester 2/JAVA/Tonmoy/LAB_10/Task_5/src/Start.java
@@ -1,10 +1,13 @@
 public class Start {
     public static void main(String[] args) {
         Shape objRef = new Circle(5.0);
+//        objRef.drawShape();  // task 5
         objRef.calculateArea();
         objRef = new Triangle(5.0,4.0);
+//        objRef.drawShape(); // task 5
         objRef.calculateArea();
         objRef = new Square(5.0);
+//        objRef.drawShape(); // task 5
         objRef.calculateArea();
     }
 }
