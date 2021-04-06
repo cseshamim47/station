9590e7b13535d923c227ca3ce2e80cd70243e8ef
Md Shamim Ahmed <cseshamim47@gmail.com>
2021-04-06 17:38:49 +0600

    Last git for ugit

--- AIUB/semester 2/JAVA/Tonmoy/LAB_10/Task_5/src/Start.java
+++ AIUB/semester 2/JAVA/Tonmoy/LAB_10/Task_5/src/Start.java
@@ -1,13 +1,13 @@
 public class Start {
     public static void main(String[] args) {
-        Shape objRef = new Circle(5.0);
-//        objRef.drawShape();  // task 5
+        Shape objRef = new Circle(15.0);
+        objRef.drawShape();  // task 5
         objRef.calculateArea();
-        objRef = new Triangle(5.0,4.0);
-//        objRef.drawShape(); // task 5
+        objRef = new Triangle(12.0,4.0);
+        objRef.drawShape(); // task 5
         objRef.calculateArea();
-        objRef = new Square(5.0);
-//        objRef.drawShape(); // task 5
+        objRef = new Square(6.0);
+        objRef.drawShape(); // task 5
         objRef.calculateArea();
     }
 }
--- AIUB/semester 2/JAVA/Tonmoy/LAB_10/Task_6/src/InterFaceDemo.java
+++ AIUB/semester 2/JAVA/Tonmoy/LAB_10/Task_6/src/InterFaceDemo.java
@@ -1,8 +1,8 @@
 public class InterFaceDemo implements IHello, IWelcome{
     public void print(){
-        System.out.println("Hello");
+        System.out.println("Hello Rabbi");
     }
     public void welcome(){
-        System.out.println("Welcome");
+        System.out.println("Welcome Rabbi");
     }
 }
