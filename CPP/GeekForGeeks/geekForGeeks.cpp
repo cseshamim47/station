5a3d70c701fac70b0576672004312f001e27eedd
Shamim Ahmed <cseshamim47@gmail.com>
2021-05-22 00:01:02 +0600

    code

--- CPP/GeekForGeeks/geekForGeeks.cpp
+++ CPP/GeekForGeeks/geekForGeeks.cpp
@@ -1,31 +1,26 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-struct S1{
-    char c1; //  1 + 7
-    double d1; // 8
-    char c2; // 1 + 7
-}__attribute__((packed));
-
-struct S2{
-    char c1; // 1
-    char c2; // 1 + 6
-    double d1; // 8
-}__attribute__((packed));
-
-struct S3{
-    double d1; // 8 
-    char c1; // 1
-    char c2; // 1 + 6
-}__attribute__((packed));
-
-
 int main()
 {
+    int size, *p;
+    scanf("%d",&size);
 
-    cout << sizeof(S1) << ' '
-         << sizeof(S2) << ' '
-         << sizeof(S3) << ' ';
-    
+    p = new int[size];
+
+    for(int i = 0; i < size; i++){
+        scanf("%d", &p[i]);
+    }
+
+    for(int i = 0; i < size; i++) printf("%d ", p[i]);
+
+    // scanf("%d",&size);
+
+    // p = new int[size];
+
+    for(int i = 3; i < size+2; i++){
+        scanf("%d", &p[i]);
+    }
+
+    for(int i = 0; i < size; i++) printf("%d ", p[i]);
 }
